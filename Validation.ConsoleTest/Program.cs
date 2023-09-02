03d08b971c0dbd5414dd5591fbab660bed4a8baa
Starinin Andrey <starininandrey@gmail.com>
2023-09-02 12:02:05 +0300

    Initial commit

---
+++ Validation.ConsoleTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using Validation;
+
+var regexp = @"^[0-9]+$";
+
+var str = "sdfsdfgsdd 54154sdfgsdgf 54154 asdgfasdgf";
+var numbers = "123123123";
+
+var result = str.Validation(regexp);
+Console.WriteLine($"{str} -> valid : {result}");
+
+result = numbers.Validation(regexp);
+Console.WriteLine($"{numbers} -> valid : {result}");
+
+PrintValid(numbers, s => s.Validation(regexp));
+PrintValid(str, s => s.Validation(regexp));
+
+PrintValid(numbers, str.Validation);
+return;
+
+void PrintValid(string str, Predicate<string> predicate)
+{
+    if (predicate.Invoke(str))
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"{str} is valid");
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{str} is not valid");
+    }
+    Console.ResetColor();
+}
---
+++ Validation/ValidationClass.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Validation;
+
+public static class ValidationClass
+{
+    public static bool Validation(this string str, string regExp)
+    {
+        var regexp = new Regex(regExp);
+        return regexp.IsMatch(str);
+    }
+}
